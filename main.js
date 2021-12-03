a0acc8e597444b32262dc146030edd3c23e81fb9
Olga-front <okombarova@icloud.com>
2021-12-03 14:09:45 +0100

    commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const wrapPage = document.querySelector('.wrap-page');
   const header = document.querySelector('.header.js-header');
   const headerIcons = document.querySelector('.header__icons');
+  const headerMenuMobile = document.querySelector('.header-menu-mobile');
+  const headerMenuMobileSocial = document.querySelector('.header-menu-mobile__social');
   const head = document.head || document.getElementsByTagName('head')[0];
   const projectsContent = document.querySelector('.projects-content');
   
@@ -22,22 +24,55 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const smoothScroll = () => {
     let anchorlinks = document.querySelectorAll('.scroll-to');
 
-    for (let item of anchorlinks) {
-      item.addEventListener('click', (e) => {
-        let hashval = item.getAttribute('href');
-
-        let target = document.querySelector(hashval);
-
-        target.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-        });
-
-        history.pushState(null, null, hashval);
-
-        e.preventDefault();
-      })
+    for (const link of anchorlinks) {
+      link.addEventListener('click', clickHandler);
     }
+
+    function clickHandler(e) {
+      e.preventDefault();
+
+      const href = this.getAttribute("href");
+      const offsetTop = document.querySelector(href).offsetTop;
+
+      scroll({
+        top: offsetTop,
+        behavior: "smooth"
+      });
+    }
+
+    // for (const item of anchorlinks) {
+    //   item.addEventListener('click', (e) => {
+    //     let hashval = item.getAttribute('href');
+
+    //     let target = document.querySelector(hashval);
+
+    //     target.scrollIntoView({
+    //       behavior: 'smooth',
+    //       block: 'start'
+    //     });
+
+    //     history.pushState(null, null, hashval);
+
+    //     e.preventDefault();
+    //   })
+    // }
+
+    // const links = document.querySelectorAll(".page-header ul a");
+
+    // for (const link of links) {
+    //   link.addEventListener("click", clickHandler);
+    // }
+
+    // function clickHandler(e) {
+    //   e.preventDefault();
+    //   const href = this.getAttribute("href");
+    //   const offsetTop = document.querySelector(href).offsetTop;
+
+    //   scroll({
+    //     top: offsetTop,
+    //     behavior: "smooth"
+    //   });
+    // }
   }
 
   const stylesheet = document.createElement('link');
@@ -50,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   realisteWidgetScript.src = 'https://script.realiste-widget.ru/widget-init.js';
   
   const realisteWidgetWrap = document.createElement('div');
-  realisteWidgetWrap.className = 'realiste-widget-wrap';
+  realisteWidgetWrap.className = 'widget-wrap';
   realisteWidgetWrap.id = 'realisteWidgetWrap';
   realisteWidgetWrap.innerHTML = '<div id="realisteWidget" data-widget="https://mr-group.realiste.io/#/"></div>';
 
@@ -60,6 +95,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   tradeInButton.innerHTML = 'Trade In';
 
   header.insertBefore(tradeInButton, headerIcons);
+  headerMenuMobile.insertBefore(tradeInButton, headerMenuMobileSocial);
   wrapPage.append(realisteWidgetWrap);
   head.appendChild(realisteWidgetScript);
   head.appendChild(stylesheet);
