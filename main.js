329c484ea9281e3ea71c8e30ca77d91aaf5f14fd
Olga-front <okombarova@icloud.com>
2021-12-03 13:41:40 +0100

    commit

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const projectsContent = document.querySelector('.projects-content');
   
   const tradeUpColumns = document.createElement('div');
-  tradeUpColumns.className = 'realiste-trade-up-columns';
-  tradeUpColumns.innerHTML = `<div class="realiste-trade-up-col">
+  tradeUpColumns.className = 'trade-up-columns';
+  tradeUpColumns.innerHTML = `<div class="trade-up-col">
     <img src="https://olga-front.github.io/mr-group.widget-demo/images//trade-up-before.jpg" alt="Trade up before">
   </div>
-  <div class="realiste-trade-up-card">
-    <h2>Обмен вашей квартиры на новую</h2>
-    <p>Выгодно обменяйте вашу квартиру на новую за 7 дней. Оценка вашей квартиры займет 10 секунд, подбор новой квартиры с минимальной разницей в цене или без доплаты займет 1 минуту.</p>
-    <a href="#realisteWidgetWrap" class="btn btn-realiste-secondary">Получить предложение за 10 сек</a>
+  <div class="trade-up-card">
+    <h2>Обмен вашей квартиры на&nbsp;новую</h2>
+    <p class="lead">Выгодно обменяйте вашу квартиру на новую за 7 дней. Оценка вашей квартиры займет 10 секунд, подбор новой квартиры с минимальной разницей в цене или без доплаты займет 1 минуту.</p>
+    <a href="#realisteWidgetWrap" class="btn btn-no-transition btn-secondary scroll-to">Получить предложение за 10 сек</a>
   </div>
-  <div class="realiste-trade-up-col">
+  <div class="trade-up-col">
     <img src="https://olga-front.github.io/mr-group.widget-demo/images//trade-up-after.jpg" alt="Trade up after">
   </div>`;
 
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   realisteWidgetWrap.innerHTML = '<div id="realisteWidget" data-widget="https://mr-group.realiste.io/#/"></div>';
 
   const tradeInButton = document.createElement('a');
-  tradeInButton.className = 'btn btn-trade-in scroll-to';
+  tradeInButton.className = 'btn btn-no-transition btn-trade-in scroll-to';
   tradeInButton.href = '#realisteWidgetWrap';
   tradeInButton.innerHTML = 'Trade In';
 
