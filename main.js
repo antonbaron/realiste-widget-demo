04d5c3e8e116c4501e1c08199dc039395b7c02fa
Olga-front <okombarova@icloud.com>
2021-12-03 13:18:13 +0100

    commit

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const header = document.querySelector('.header.js-header');
   const headerIcons = document.querySelector('.header__icons');
   const head = document.head || document.getElementsByTagName('head')[0];
+  const projectsContent = document.querySelector('.projects-content');
+  
+  const tradeUpColumns = document.createElement('div');
+  tradeUpColumns.className = 'realiste-trade-up-columns';
+  tradeUpColumns.innerHTML = `<div class="realiste-trade-up-col">
+    <img src="https://olga-front.github.io/mr-group.widget-demo/images//trade-up-before.jpg" alt="Trade up before">
+  </div>
+  <div class="realiste-trade-up-card">
+    <h2>Обмен вашей квартиры на новую</h2>
+    <p>Выгодно обменяйте вашу квартиру на новую за 7 дней. Оценка вашей квартиры займет 10 секунд, подбор новой квартиры с минимальной разницей в цене или без доплаты займет 1 минуту.</p>
+    <a href="#realisteWidgetWrap" class="btn btn-realiste-secondary">Получить предложение за 10 сек</a>
+  </div>
+  <div class="realiste-trade-up-col">
+    <img src="https://olga-front.github.io/mr-group.widget-demo/images//trade-up-after.jpg" alt="Trade up after">
+  </div>`;
 
   const smoothScroll = () => {
     let anchorlinks = document.querySelectorAll('.scroll-to');
@@ -48,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   wrapPage.append(realisteWidgetWrap);
   head.appendChild(realisteWidgetScript);
   head.appendChild(stylesheet);
+  projectsContent.append(tradeUpColumns);
 
   smoothScroll();
 
