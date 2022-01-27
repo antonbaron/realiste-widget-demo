efab23f95b6dc3bae6bc75fb181b8fd5b6364345
Olga-front <okombarova@icloud.com>
2022-01-27 10:29:23 +0100

    commit

--- js/modules/alfabank.js
+++ js/modules/alfabank.js
@@ -1,25 +1,41 @@
-import { stylesheet, stylesheetExists, detectMobile } from './common.js';
+import { stylesheet, stylesheetExists, detectMobile, findAndReplaceText } from './common.js';
 
 const initElementsAlfabank = () => {
-  document.body.innerHTML = document.body.innerHTML.replace(/Альфа-|АЛЬФА-/g, "");
+  const realisteWidgetWrap = document.createElement('div');
+  realisteWidgetWrap.className = 'widget-wrap alfabank';
+  realisteWidgetWrap.id = 'realisteWidgetWrap';
+  
+  realisteWidgetWrap.innerHTML = `<div class="b-block-text container">
+    <p class="widget-wrap-title">Купить и обменять Вашу квартиру</p>
+    <div id="realisteWidget" data-widget="https://alfabank.realiste.io/trade-up"></div>
+  </div>`;
 
-  if (stylesheetExists(stylesheet.href)) {
+  const realisteWidgetBtn = document.createElement('a');
+  realisteWidgetBtn.href = '#realisteWidgetWrap';
+  realisteWidgetBtn.className = 'realiste-widget-btn';
+
+  realisteWidgetBtn.innerHTML = `Обмен вашей квартиры`;
+
+  let interval;
+
+  const init = () => {
     const referenceNode = !detectMobile() ? document.querySelector('#benefit.a2op2t.s2op2t') : document.querySelector('#alfa > div > .c3iFBg[data-widget-name=Layout]');
     const headerLogo = !detectMobile() ? document.querySelector('.j1Cda9') : document.querySelector('.d3iFBg.d1S2QV.b1S2QV.i1S2QV');
+    const btnReferenceNode = !detectMobile() ? document.querySelector('.a1rN1L.h1rN1L.b1rN1L.i1rN1L.c3FBX4.d3FBX4[data-test-id=tabs-list-tabTitle-0]') : document.querySelector('.a1rN1L.h1rN1L.b1rN1L.i1rN1L.c1NPQw.d1NPQw[data-test-id=tabs-list-tabTitle-0]');
 
-    const realisteWidgetWrap = document.createElement('div');
-    realisteWidgetWrap.className = 'widget-wrap alfabank';
-    realisteWidgetWrap.id = 'realisteWidgetWrap';
-    
-    realisteWidgetWrap.innerHTML = `<div class="b-block-text container">
-      <p class="widget-wrap-title">Купить и обменять Вашу квартиру</p>
-      <div id="realisteWidget" data-widget="https://alfabank.realiste.io/trade-up"></div>
-    </div>`;
+    if (stylesheetExists(stylesheet.href) && referenceNode && headerLogo && btnReferenceNode) {
+      clearInterval(interval);
 
-    headerLogo.style.display = 'none';
+      findAndReplaceText(new RegExp('Альфа-|АЛЬФА-', "g"), '');
 
-    if (referenceNode) referenceNode.parentNode.insertBefore(realisteWidgetWrap, referenceNode.nextSibling);
-  } else setTimeout(initElementsRaiffeisen, 100);
+      headerLogo.style.display = 'none';
+
+      if (referenceNode) referenceNode.parentNode.insertBefore(realisteWidgetWrap, referenceNode.nextSibling);
+      if (btnReferenceNode) btnReferenceNode.parentNode.insertBefore(realisteWidgetBtn, btnReferenceNode.nextSibling);
+    }
+  }
+
+  interval = setInterval(init, 1000);
 }
 
 export default initElementsAlfabank;
--- js/modules/common.js
+++ js/modules/common.js
@@ -32,3 +32,29 @@ export const detectMobile = () => {
 
   return checkRes;
 }
+
+// find and replace specific text characters across a document
+
+const nativeSelector = () => {
+  let elements = document.querySelectorAll('body, body *');
+  let results = [];
+  let child;
+  for(var i = 0; i < elements.length; i++) {
+    child = elements[i].childNodes[0];
+    if (elements[i].hasChildNodes() && child.nodeType == 3) {
+      results.push(child);
+    }
+  }
+  return results;
+}
+
+export const findAndReplaceText = (from, to) => {
+  let textnodes = nativeSelector();
+  let _nv;
+  for (let i = 0, len = textnodes.length; i < len; i++){
+    _nv = textnodes[i].nodeValue;
+    textnodes[i].nodeValue = _nv.replace(from, to);
+  }
+}
+
+// new RegExp(oldWord, "g")
