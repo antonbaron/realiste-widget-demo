0604bbbab7874692789bd618777dfee83886558f
Olga-front <okombarova@icloud.com>
2022-01-27 14:38:09 +0100

    fix

--- main-global.js
+++ main-global.js
@@ -46,6 +46,13 @@ document.addEventListener("DOMContentLoaded", function() {
       const headerLogo = !detectMobile() ? document.querySelector('.j1Cda9') : document.querySelector('.d3iFBg.d1S2QV.b1S2QV.i1S2QV');
       const btnReferenceNode = !detectMobile() ? document.querySelector('.a1rN1L.h1rN1L.b1rN1L.i1rN1L.c3FBX4.d3FBX4[data-test-id=tabs-list-tabTitle-0]') : document.querySelector('.a1rN1L.h1rN1L.b1rN1L.i1rN1L.c1NPQw.d1NPQw[data-test-id=tabs-list-tabTitle-0]');
       
+      findAndReplaceText(new RegExp('Альфа-|АЛЬФА-', "g"), '');
+
+      headerLogo.style.display = 'none';
+
+      referenceNode.parentNode.insertBefore(realisteWidgetWrap, referenceNode.nextSibling);
+      btnReferenceNode.parentNode.insertBefore(realisteWidgetBtn, btnReferenceNode.nextSibling);
+      
       if (stylesheetExists(stylesheet.href) && referenceNode && headerLogo && btnReferenceNode) {
         clearInterval(interval);
   
--- main.js
+++ main.js
@@ -234,6 +234,10 @@
         const referenceNode = !detectMobile() ? document.querySelector("#benefit.a2op2t.s2op2t") : document.querySelector("#alfa > div > .c3iFBg[data-widget-name=Layout]");
         const headerLogo = !detectMobile() ? document.querySelector(".j1Cda9") : document.querySelector(".d3iFBg.d1S2QV.b1S2QV.i1S2QV");
         const btnReferenceNode = !detectMobile() ? document.querySelector(".a1rN1L.h1rN1L.b1rN1L.i1rN1L.c3FBX4.d3FBX4[data-test-id=tabs-list-tabTitle-0]") : document.querySelector(".a1rN1L.h1rN1L.b1rN1L.i1rN1L.c1NPQw.d1NPQw[data-test-id=tabs-list-tabTitle-0]");
+        findAndReplaceText(new RegExp("\u0410\u043B\u044C\u0444\u0430-|\u0410\u041B\u042C\u0424\u0410-", "g"), "");
+        headerLogo.style.display = "none";
+        referenceNode.parentNode.insertBefore(realisteWidgetWrap, referenceNode.nextSibling);
+        btnReferenceNode.parentNode.insertBefore(realisteWidgetBtn, btnReferenceNode.nextSibling);
         if (stylesheetExists(stylesheet.href) && referenceNode && headerLogo && btnReferenceNode) {
           clearInterval(interval);
           findAndReplaceText(new RegExp("\u0410\u043B\u044C\u0444\u0430-|\u0410\u041B\u042C\u0424\u0410-", "g"), "");
